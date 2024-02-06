4e26808e1e96056f2e8de5408f76a2e3d5ed15e5
jarltuxen <jarl.tuxen@gmail.com>
2024-02-06 13:49:47 +0100

    added text

--- src/dk/kea/Main.java
+++ src/dk/kea/Main.java
@@ -23,12 +23,15 @@ public class Main {
     public static void main(String[] args) {
         Integer[] duplicateArray = {7, 4, -9, 4, 15, 8, 27, 7, 11, -5, 32, -9, -9};
 	    List<Integer> duplicates = new LinkedList<>(List.of(duplicateArray));
+        System.out.println("Sort and Remove Duplicates");
         System.out.println("Tal: " + duplicates);
         sortAndRemoveDuplicates(duplicates);
         System.out.println("Sorteret uden kopier " + duplicates);
+        System.out.println();
 
         Integer[] notUnique = {3, 7, 3, -1, 2, 3, 7, 2, 15, 15};
         List<Integer> notUniqueList = new LinkedList<>(List.of(notUnique));
+        System.out.println("Count Unique");
         System.out.println("Tal: " + notUniqueList);
         System.out.println("Unikke tal: " + countUnique(notUniqueList));
 
